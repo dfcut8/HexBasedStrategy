15feeb67c8dfbdc507f2401419a238bd6a9bde5c
dfcut8 <dfcut8@proton.me>
2026-05-20 11:01:10 -0400

    better code organization

--- HexTileMap.cs
+++ HexTileMap.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Godot;
 using HexBasedStrategy.Core;
 
@@ -14,21 +13,22 @@ public partial class HexTileMap : Node2D
     [Export]
     public int Height = 20;
 
-    private TileMapLayer BaseLayer;
-    private TileMapLayer BorderLayer;
-    private TileMapLayer OverlayLayer;
+    private TileMapLayer BaseLayer = null!;
+    private TileMapLayer BorderLayer = null!;
+    private TileMapLayer OverlayLayer = null!;
 
-    private Dictionary<Vector2I, Hex> mapData = [];
+    private readonly Dictionary<Vector2I, Hex> mapData = [];
+
     private readonly Dictionary<TerrainType, Vector2I> terrainToTextureCoords = new()
     {
-        [TerrainType.Plains] = new Vector2I(0, 0),
-        [TerrainType.Desert] = new Vector2I(0, 1),
-        [TerrainType.Beach] = new Vector2I(0, 2),
-        [TerrainType.Ice] = new Vector2I(0, 3),
-        [TerrainType.Water] = new Vector2I(1, 0),
-        [TerrainType.Mountain] = new Vector2I(1, 1),
-        [TerrainType.Shallows] = new Vector2I(1, 2),
-        [TerrainType.Forest] = new Vector2I(1, 3),
+        [TerrainType.Plains] = new(0, 0),
+        [TerrainType.Desert] = new(0, 1),
+        [TerrainType.Beach] = new(0, 2),
+        [TerrainType.Ice] = new(0, 3),
+        [TerrainType.Water] = new(1, 0),
+        [TerrainType.Mountain] = new(1, 1),
+        [TerrainType.Shallows] = new(1, 2),
+        [TerrainType.Forest] = new(1, 3),
     };
 
     public override void _Ready()
@@ -38,10 +38,9 @@ public partial class HexTileMap : Node2D
         OverlayLayer = GetNode<TileMapLayer>("OverlayLayer");
 
         GenerateTerrain();
+
         GD.Print("HexMap Ready!");
     }
-
-    public override void _Process(double delta) { }
 
     public Vector2 MapToLocal(Vector2I coords)
     {
@@ -50,74 +49,60 @@ public partial class HexTileMap : Node2D
 
     private void GenerateTerrain()
     {
-        float[,] noiseMap = new float[Width, Height];
-        float[,] forestMap = new float[Width, Height];
-        float[,] desertMap = new float[Width, Height];
-        float[,] mountainMap = new float[Width, Height];
+        var noise = CreateNoise();
+        var values = new float[Width, Height];
 
-        var r = new Random();
-        int seed = r.Next(100_000);
+        float max = 0f;
 
-        // Base terrain (water, Beach, Plains)
-        var noise = new FastNoiseLite
+        for (int x = 0; x < Width; x++)
         {
-            Seed = seed,
+            for (int y = 0; y < Height; y++)
+            {
+                float value = Math.Abs(noise.GetNoise2D(x, y));
+                values[x, y] = value;
+                max = Math.Max(max, value);
+            }
+        }
+
+        for (int x = 0; x < Width; x++)
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                var coords = new Vector2I(x, y);
+                var terrain = GetBaseTerrain(values[x, y], max);
+
+                mapData[coords] = new Hex(coords) { TerrainType = terrain };
+
+                BaseLayer.SetCell(coords, 0, terrainToTextureCoords[terrain]);
+                BorderLayer.SetCell(coords, 0, Vector2I.Zero);
+            }
+        }
+
+        Callable.From(() => GlobalEvents.MapGenerationCompleted?.Invoke(this)).CallDeferred();
+    }
+
+    private static FastNoiseLite CreateNoise()
+    {
+        return new FastNoiseLite
+        {
+            Seed = Random.Shared.Next(100_000),
             Frequency = 0.008f,
             FractalType = FastNoiseLite.FractalTypeEnum.Fbm,
             FractalOctaves = 4,
             FractalLacunarity = 2.25f,
         };
+    }
 
-        var noiseMax = 0f;
+    private static TerrainType GetBaseTerrain(float value, float max)
+    {
+        float normalized = value / max;
 
-        // Generator
-        for (int x = 0; x < Width; x++)
+        return normalized switch
         {
-            for (int y = 0; y < Height; y++)
-            {
-                noiseMap[x, y] = Math.Abs(noise.GetNoise2D(x, y));
-                if (noiseMap[x, y] > noiseMax)
-                {
-                    noiseMax = noiseMap[x, y];
-                }
-            }
-        }
-
-        List<(float Min, float Max, TerrainType Type)> terrainGeneratedValues =
-        [
-            (0, noiseMax / 10 * 2.5f, TerrainType.Water),
-            (noiseMax / 10 * 2.5f, noiseMax / 10 * 4f, TerrainType.Shallows),
-            (noiseMax / 10 * 4f, noiseMax / 10 * 4.5f, TerrainType.Beach),
-            (noiseMax / 10 * 4.5f, noiseMax + 0.05f, TerrainType.Plains),
-        ];
-
-        for (int x = 0; x < Width; x++)
-        {
-            for (int y = 0; y < Height; y++)
-            {
-                Hex h = new(new Vector2I(x, y));
-                float noiseValue = noiseMap[x, y];
-                try
-                {
-                    h.TerrainType = terrainGeneratedValues
-                        .First(range => noiseValue >= range.Min && noiseValue < range.Max)
-                        .Type;
-                    mapData[new Vector2I(x, y)] = h;
-                }
-                catch (Exception e)
-                {
-                    GD.Print(e);
-                }
-                BaseLayer.SetCell(new Vector2I(x, y), 0, terrainToTextureCoords[h.TerrainType]);
-                //BaseLayer.SetCell(new Vector2I(x, y), 0, new Vector2I(0, 0));
-                BorderLayer.SetCell(new Vector2I(x, y), 0, new Vector2I(0, 0));
-            }
-        }
-        Callable
-            .From(() =>
-            {
-                GlobalEvents.MapGenerationCompleted?.Invoke(this);
-            })
-            .CallDeferred();
+            < 0.25f => TerrainType.Water,
+            < 0.40f => TerrainType.Shallows,
+            < 0.45f => TerrainType.Beach,
+            _ => TerrainType.Plains,
+        };
     }
 }
