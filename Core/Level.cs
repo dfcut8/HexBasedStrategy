850f50d50d496eb9e8d5bc6e3fa5d5241ebb1aea
dfcut8 <dfcut8@proton.me>
2026-06-18 18:07:03 -0400

    continue refactoring

---
+++ Core/Level.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Godot;
+using HexBasedStrategy.Objects;
+
+namespace HexBasedStrategy.Core;
+
+public partial class Level
+{
+    [Export]
+    private List<CivilizationData> CivilizationsDate { get; set; } = [];
+    public List<Civilization> Civilizations { get; set; } = [];
+    public Dictionary<Vector2I, City> coordsToCities = [];
+
+    public void CreateCivilizations()
+    {
+        Civi
+    }
+}
