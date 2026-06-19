46df254aba9cfc4942d3df2f73ced102829442c3
dfcut8 <dfcut8@proton.me>
2026-06-19 08:31:55 -0400

    add vscode and fix level civilization data

--- Core/Level.cs
+++ Core/Level.cs
@@ -5,12 +5,19 @@ using HexBasedStrategy.Objects;
 
 namespace HexBasedStrategy.Core;
 
-public partial class Level
+public partial class Level : Node
 {
     [Export]
-    private List<CivilizationData> CivilizationsDate { get; set; } = [];
+    private CivilizationData[] CivilizationDataList { get; set; } = [];
+
     public List<Civilization> Civilizations { get; set; } = [];
     public Dictionary<Vector2I, City> coordsToCities = [];
 
-    public void CreateCivilizations() { }
+    public override void _Ready()
+    {
+        foreach (var cd in CivilizationDataList)
+        {
+            Civilizations.Add(new Civilization() { Color = cd.Color, Name = cd.Name });
+        }
+    }
 }
