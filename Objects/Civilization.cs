55b30448537c288dc1eaa09a6a39788958cf70b2
dfcut8 <dfcut8@proton.me>
2026-06-22 11:08:57 -0400

    fixed civ init

--- Objects/Civilization.cs
+++ Objects/Civilization.cs
@@ -12,19 +12,23 @@ public class Civilization
     public Color Color { get; set; } = Colors.White;
     public string Name { get; set; } = String.Empty;
     public bool PlayerControlled { get; set; } = false;
-    public HashSet<string> CityNames { get; init; } = [];
+    public HashSet<string> CityNames
+    {
+        get;
+        init
+        {
+            field = value;
+            availableCityNames = [.. value];
+        }
+    } = [];
 
     private HashSet<string> availableCityNames = [];
 
-    public Civilization()
-    {
-        availableCityNames = CityNames;
-    }
+    public Civilization() { }
 
     public string GetAndRemoveRandomCityNameFromAvailableCityNames()
     {
-        var r = new Random();
-        var i = r.Next(availableCityNames.Count);
+        var i = Random.Shared.Next(availableCityNames.Count);
         var name = availableCityNames.ElementAt(i);
         availableCityNames.Remove(name);
         return name;
