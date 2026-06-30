872668b9ec9852ce84cacacf78c23251008dd02d
dfcut8 <dfcut8@proton.me>
2026-06-30 16:25:08 -0400

    some iteration on the thingy

--- Systems/CityGrowthResource.cs
+++ Systems/CityGrowthResource.cs
@@ -1,5 +1,25 @@
 using System;
 using Godot;
+using HexBasedStrategy.Systems;
 
 [GlobalClass]
-public partial class CityGrowthResource : Resource { }
+public partial class CityGrowthResource : Resource
+{
+    [Export]
+    private CityGrowthSystemType cityGrowthSystemType;
+
+    public enum CityGrowthSystemType
+    {
+        RandomTile,
+        Complex,
+    }
+
+    public ICityGrowthSystem? GetInstance()
+    {
+        return cityGrowthSystemType switch
+        {
+            CityGrowthSystemType.RandomTile => new CityGrowthSystemRandomTile(),
+            _ => null,
+        };
+    }
+}
