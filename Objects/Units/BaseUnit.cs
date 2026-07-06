bd99122815d0dbf9e5fe40502b39ac046aaf35fa
dfcut8 <dfcut8@proton.me>
2026-07-06 10:19:57 -0400

    small refactoring for composition

--- Objects/Units/BaseUnit.cs
+++ Objects/Units/BaseUnit.cs
@@ -1,10 +1,24 @@
 using System;
 using Godot;
+using HexBasedStrategy.Data.Units;
 
 namespace HexBasedStrategy.Objects.Units;
 
 public partial class BaseUnit : Node2D
 {
     [Export]
-    public string UnitName { get; set; } = string.Empty;
+    public required UnitData Data { get; set; }
+
+    private Sprite2D? sprite;
+
+    public override void _Ready()
+    {
+        if (Data is null)
+        {
+            GD.PrintErr("Unit data should be attached to unit");
+            GetTree().Quit(1);
+        }
+        sprite = GetNode<Sprite2D>("Sprite2D");
+        sprite.Texture = Data?.UnitTexture;
+    }
 }
--- Objects/Units/Warrior.cs
+++
@@ -1,6 +0,0 @@
-using System;
-using Godot;
-
-namespace HexBasedStrategy.Objects.Units;
-
-public partial class Warrior : BaseUnit { }
